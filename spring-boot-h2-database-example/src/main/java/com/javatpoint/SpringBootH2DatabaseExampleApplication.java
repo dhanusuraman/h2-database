b825670ebb54c7ff51c479925f30d8226bf83c25
Ram <ramsara74@gmail.com>
2021-01-07 13:26:21 +0530

    comment line

--- spring-boot-h2-database-example/src/main/java/com/javatpoint/SpringBootH2DatabaseExampleApplication.java
+++ spring-boot-h2-database-example/src/main/java/com/javatpoint/SpringBootH2DatabaseExampleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootH2DatabaseExampleApplication {
-    //Main Method
+    //Main Method Execution
     public static void main(String[] args) {
         SpringApplication.run(SpringBootH2DatabaseExampleApplication.class, args);
     }
