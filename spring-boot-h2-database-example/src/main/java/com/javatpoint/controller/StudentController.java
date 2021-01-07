d5c2638022c9573113e85fcc985d16057c034e01
Ram <ramsara74@gmail.com>
2021-01-07 16:11:25 +0530

    Formated

--- spring-boot-h2-database-example/src/main/java/com/javatpoint/controller/StudentController.java
+++ spring-boot-h2-database-example/src/main/java/com/javatpoint/controller/StudentController.java
@@ -12,36 +12,34 @@ import org.springframework.web.bind.annotation.RestController;
 import com.javatpoint.model.Student;
 import com.javatpoint.service.StudentService;
 
-//creating RestController
+// creating RestController
 @RestController
 public class StudentController {
-    //autowired the StudentService class
-    @Autowired
-    private
-    StudentService studentService;
+  // autowired the StudentService class
+  @Autowired private StudentService studentService;
 
-    //creating a get mapping that retrieves all the students detail from the database
-    @GetMapping("/student")
-    public List<Student> getAllStudent() {
-        return studentService.getAllStudent();
-    }
+  // creating a get mapping that retrieves all the students detail from the database
+  @GetMapping("/student")
+  public List<Student> getAllStudent() {
+    return studentService.getAllStudent();
+  }
 
-    //creating a get mapping that retrieves the detail of a specific student
-    @GetMapping("/student/{id}")
-    public Student getStudent(@PathVariable("id") int id) {
-        return studentService.getStudentById(id);
-    }
+  // creating a get mapping that retrieves the detail of a specific student
+  @GetMapping("/student/{id}")
+  public Student getStudent(@PathVariable("id") int id) {
+    return studentService.getStudentById(id);
+  }
 
-    //creating a delete mapping that deletes a specific student
-    @DeleteMapping("/student/{id}")
-    public void deleteStudent(@PathVariable("id") int id) {
-        studentService.delete(id);
-    }
+  // creating a delete mapping that deletes a specific student
+  @DeleteMapping("/student/{id}")
+  public void deleteStudent(@PathVariable("id") int id) {
+    studentService.delete(id);
+  }
 
-    //creating post mapping that post the student detail in the database
-    @PostMapping("/student")
-    public int saveStudent(@RequestBody Student student) {
-        studentService.saveOrUpdate(student);
-        return student.getId();
-    }
+  // creating post mapping that post the student detail in the database
+  @PostMapping("/student")
+  public int saveStudent(@RequestBody Student student) {
+    studentService.saveOrUpdate(student);
+    return student.getId();
+  }
 }
--- spring-boot-h2-database-example/src/main/java/com/javatpoint/model/Student.java
+++ spring-boot-h2-database-example/src/main/java/com/javatpoint/model/Student.java
@@ -5,55 +5,52 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-//mark class as an Entity
+// mark class as an Entity
 @Entity
-//defining class name as Table name
+// defining class name as Table name
 @Table
 public class Student {
-    //mark id as primary key
-    @Id
-//defining id as column name
-    @Column
-    private int id;
-    //defining name as column name
-    @Column
-    private String name;
-    //defining age as column name
-    @Column
-    private int age;
-    //defining email as column name
-    @Column
-    private String email;
+  // mark id as primary key
+  @Id
+  // defining id as column name
+  @Column
+  private int id;
+  // defining name as column name
+  @Column private String name;
+  // defining age as column name
+  @Column private int age;
+  // defining email as column name
+  @Column private String email;
 
-    public int getId() {
-        return id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public int getAge() {
-        return age;
-    }
+  public int getAge() {
+    return age;
+  }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+  public void setAge(int age) {
+    this.age = age;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 }
