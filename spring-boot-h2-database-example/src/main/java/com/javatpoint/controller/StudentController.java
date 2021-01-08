04652cb91ac70dff97949afd0a87a4d72e8363dd
Ram <ramsara74@gmail.com>
2021-01-08 10:57:57 +0530

    sonar

--- spring-boot-h2-database-example/src/main/java/com/javatpoint/controller/StudentController.java
+++ spring-boot-h2-database-example/src/main/java/com/javatpoint/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.javatpoint.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,36 +13,34 @@ import org.springframework.web.bind.annotation.RestController;
 import com.javatpoint.model.Student;
 import com.javatpoint.service.StudentService;
 
-//creating RestController
+// creating RestController
 @RestController
 public class StudentController {
-    //autowired the StudentService class
-    @Autowired
-    private
-    StudentService studentService;
+  // autowired the StudentService class
+  @Autowired private StudentService studentService;
 
-    //creating a get mapping that retrieves all the students detail from the database
-    @GetMapping("/student")
-    public List<Student> getAllStudent() {
-        return studentService.getAllStudent();
-    }
+  // creating a get mapping that retrieves all the students detail from the database
+  @GetMapping("/student")
+  public List<Student> getAllStudent() {
+    return studentService.getAllStudent();
+  }
 
-    //creating a get mapping that retrieves the detail of a specific student
-    @GetMapping("/student/{id}")
-    public Student getStudent(@PathVariable("id") int id) {
-        return studentService.getStudentById(id);
-    }
+  // creating a get mapping that retrieves the detail of a specific student
+  @GetMapping("/student/{id}")
+  public Optional<Student> getStudent(@PathVariable("id") int id) {
+    return studentService.getStudentById(id);
+  }
 
-    //creating a delete mapping that deletes a specific student
-    @DeleteMapping("/student/{id}")
-    public void deleteStudent(@PathVariable("id") int id) {
-        studentService.delete(id);
-    }
+  // creating a delete mapping that deletes a specific student
+  @DeleteMapping("/student/{id}")
+  public void deleteStudent(@PathVariable("id") int id) {
+    studentService.delete(id);
+  }
 
-    //creating post mapping that post the student detail in the database
-    @PostMapping("/student")
-    public int saveStudent(@RequestBody Student student) {
-        studentService.saveOrUpdate(student);
-        return student.getId();
-    }
+  // creating post mapping that post the student detail in the database
+  @PostMapping("/student")
+  public int saveStudent(@RequestBody Student student) {
+    studentService.saveOrUpdate(student);
+    return student.getId();
+  }
 }
